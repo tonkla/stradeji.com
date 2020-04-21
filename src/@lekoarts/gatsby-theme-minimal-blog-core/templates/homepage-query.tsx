b230850630c44ef407e2426379f51f73b6a6d5d8
Surakarn Samkaew <tonkla@gmail.com>
2020-04-21 16:36:10 +0700

    Refactor homepage query

--- src/@lekoarts/gatsby-theme-minimal-blog-core/templates/homepage-query.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog-core/templates/homepage-query.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import HomepageComponent from '../../gatsby-theme-minimal-blog/components/homepage'
 
-export const query = graphql`
-  query {
-    allPost(sort: { fields: date, order: DESC }, limit: 10) {
-      nodes {
-        slug
-        title
-        date(formatString: "DD.MM.YYYY")
-        tags {
-          name
-          slug
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allPost(sort: { fields: date, order: DESC }, limit: 10) {
+          nodes {
+            slug
+            title
+            date(formatString: "DD.MM.YYYY")
+            excerpt
+            timeToRead
+            description
+            tags {
+              name
+              slug
+            }
+          }
         }
       }
-    }
-  }
-`
-
-export default ({ data }) => {
-  return <HomepageComponent posts={data.allPost.nodes} />
-}
+    `}
+    render={(data) => <HomepageComponent posts={data.allPost.nodes} />}
+  />
+)
